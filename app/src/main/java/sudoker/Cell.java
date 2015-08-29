7478aa1f5d6d162931283c062a9594b300b56406
ooo125258 <michaelsundmlis@gmail.com>
2015-08-29 15:40:49 -0400

    jafsjl

--- app/src/main/java/sudoker/Cell.java
+++ app/src/main/java/sudoker/Cell.java
@@ -1,7 +1,81 @@
 package sudoker;
 
+import java.util.*;
+import java.lang.*;
+
 /**
  * Created by qhyu on 2015-08-28.
  */
 public class Cell {
+    private int value, row, line;
+    private Set<Integer> possibleValueList;
+    private boolean status;
+
+    public Cell(int value, int row, int line){
+        setValue(value);
+        this.row = row;
+        this.line = line;
+        this.status = false;
+    }
+
+    public Cell(int row, int line){
+        this.row = row;
+        this.line = line;
+        this.value = 0;
+        this.status = true;
+    }
+
+    public void setValue(int value){
+        if (!this.status) {
+            this.value = value;
+            this.status = false;
+        }
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public int getLine(){
+        return this.line;
+    }
+
+    public int getRow(){
+        return this.row;
+    }
+
+    public Set<Integer> getPossibleValue(){
+        return this.getPossibleValue();
+    }
+
+    public boolean addPossibleValue(int possibleValue){
+        this.possibleValueList.add(possibleValue);
+        return !this.possibleValueList.contains(possibleValue);
+    }
+
+    public boolean delPossibleValue(int possibleValue){
+        if (this.possibleValueList.contains(possibleValue)){
+            this.possibleValueList.remove(possibleValue);
+            update();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void update(){
+        if ((this.possibleValueList.size() == 1) && (this.status)) {
+            setValue((int) possibleValueList.toArray()[0]);
+            this.status = false;
+        } else {
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.possibleValueList.isEmpty();
+    }
+
+
+
 }
