d38de560b242e907d8cbe4d54591aa8baf18f916
ooo125258 <michaelsundmlis@gmail.com>
2015-08-31 17:08:59 -0400

    -f

--- app/src/main/java/sudoker/tester.java
+++ app/src/main/java/sudoker/tester.java
@@ -1,8 +1,18 @@
 package sudoker;
 
+import java.io.FileNotFoundException;
+
 /**
- * Created by qhyu on 2015-08-31.
+ * Created by Chuanqi on 2015-08-31.
  */
 public class tester {
+    public static void main(String[] args) throws FileNotFoundException {
+        String address = new String();
+        Sudoker sudoker = new Sudoker();
+        sudoker.load(address);
+        sudoker.toString();
+        sudoker.solve();
+        sudoker.toString();
 
+    }
 }
