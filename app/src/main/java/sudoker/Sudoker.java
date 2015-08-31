3c589b608daf538a632e65055a4ed4b819fad01a
sino <sinoforever@outlook.com>
2015-08-31 16:47:11 -0400

    added main solver

---
+++ app/src/main/java/sudoker/Sudoker.java
@@ -0,0 +1,24 @@
+package sudoker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by qhyu on 2015-08-31.
+ */
+public class Sudoker {
+
+    private Grid board;
+
+    public Sudoker() {
+        board = new Grid();
+    }
+
+    public void load(String address) {
+        ArrayList<ArrayList<Integer>> matrix =  new ArrayList<>();
+
+    }
+
+    public void solve() {
+
+    }
+}
---
+++ app/src/main/java/sudoker/tester.java
@@ -0,0 +1,8 @@
+package sudoker;
+
+/**
+ * Created by qhyu on 2015-08-31.
+ */
+public class tester {
+
+}
