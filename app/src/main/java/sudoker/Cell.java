5e34d090d77c4354c6fa1f9827a7eeb7752e6992
sino <sinoforever@outlook.com>
2015-09-03 16:47:38 -0400

    -f

--- app/src/main/java/sudoker/Cell.java
+++ app/src/main/java/sudoker/Cell.java
@@ -7,69 +7,41 @@ import java.util.*;
  * Created by qhyu on 2015-08-28.
  */
 public class Cell extends Observable{
-    private int value, row, col;
+    private int value;
     private Set<Integer> possibleValueList = new HashSet<Integer>();
-    private boolean status;
 
-    public Cell(int value){
-        setValue(value);
-        this.status = false;
-        for(int i = 1; i < 10; i++) {if (i != value) {this.addPossibleValue(i);}}
-    }
-
-    public Cell(){
+    public Cell() {
         this.value = 0;
-        this.status = true;
-        for(int i = 1; i < 10; i++){this.addPossibleValue(i);}
+        initializePossibleValue();
     }
 
-    public void setValue(int value){
-        if (this.status) {
-            this.value = value;
-            setChanged();
-            notifyObservers(getValue());
-            this.clearPossibleValueList();
-        }
+    public void setValue(int value) {
+    	this.value = value;
+    	setChanged();
+    	notifyObservers(getValue());
+    	this.clearPossibleValueList();
     }
-        
-    
 
-    public int getValue(){
+    public int getValue() {
         return this.value;
     }
 
-    public int getLine(){
-        return this.col;
-    }
-
-    public int getRow(){
-        return this.row;
-    }
-
-    public void cellRefresh(){
-        this.value = 0;
-        this.status = true;
-    }
-
-    public Set<Integer> getPossibleValue(){
+    public Set<Integer> getPossibleValue() {
         return this.possibleValueList;
     }
-    
-    public void setPossibleValue(Set<Integer> possibleValueList){
-    	this.possibleValueList = possibleValueList;
-    }
 
-    public void addPossibleValue(int possibleValue){
+
+    public void addPossibleValue(int possibleValue) {
         this.possibleValueList.add(possibleValue);
     }
 
-    public void delPossibleValue(int possibleValue){
-        if (this.possibleValueList.contains(possibleValue)){
+    public void delPossibleValue(int possibleValue) {
+        if (this.possibleValueList.contains(possibleValue)) {
             this.possibleValueList.remove(possibleValue);
         }
     }
 
-    public void update(){
+    public void update() {
         if (readyForChange()) {
             setValue((int) possibleValueList.toArray()[0]);
             setChanged();
@@ -77,24 +49,36 @@ public class Cell extends Observable{
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return this.getValue() == 0;
     }
     
-    public void clearPossibleValueList(){
-    	if (!isEmpty()){
+    public void clearPossibleValueList() {
+    	if (!isEmpty()) {
     		this.setPossibleValue(new HashSet<Integer>());
     		this.addPossibleValue(value);
     	}
     }
     
-    public int possibleValueNumber(){
+    public int getPossibleValueCount() {
     	return getPossibleValue().size();
     }
     
+    ///////////////////////////////////
+    //PRIVATE HELPERS BELOW THIS LINE//
+    ///////////////////////////////////
+    
     private boolean readyForChange() {
-        return (possibleValueNumber() == 1) && (this.isEmpty() );
+        return (getPossibleValueCount() == 1) && (this.isEmpty() );
     }
     
-
+    private void initializePossibleValue() {
+    	for(int i = 1; i < 10; i++) {
+        	this.addPossibleValue(i);
+        }
+    }
+    
+    private void setPossibleValue(Set<Integer> possibleValueList) {
+    	this.possibleValueList = possibleValueList;
+    }
 }
